7fda19467549b7928adb18109b4b1e440bf68730
Adithi  Vaidyanathan <114479214+Adithi19@users.noreply.github.com>
2022-09-30 09:07:42 +0000

    Problem solved

---
+++ set01/problem06.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int input();
+void compare(int a, int b, int c, int *largest);
+void output(int a, int b, int c, int largest);
+
+main() {
+  int a,b,c,largest;
+  a=input();
+  b=input();
+  c=input();
+  compare(a,b,c,&largest);
+  output(a,b,c,largest);
+}
+
+int input() {
+  int digit;
+  printf("Enter a number");
+  scanf("%d",&digit);
+  return digit;
+}
+
+void compare(int a, int b, int c, int *largest) {
+  if(a>b && a>c) {
+    *largest=a;
+  }
+  else if(b>a && b>c) {
+    *largest=b;
+  }
+  else if(c>a && c>b) {
+    *largest=c;
+  }
+}
+
+void output(int a, int b, int c, int largest) {
+  printf("The largest of %d %d and %d is %d",a,b,c,largest);
+}
