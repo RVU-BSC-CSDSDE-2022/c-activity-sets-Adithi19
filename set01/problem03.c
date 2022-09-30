0adbf35898ec80de63b0c15821167e7075544f73
Adithi  Vaidyanathan <114479214+Adithi19@users.noreply.github.com>
2022-09-30 07:34:45 +0000

    Problem solved

--- set01/problem03.c
+++ set01/problem03.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int input();
-int add(int a,int b);
-void output(int a,int b,int sum);
+int add(int a, int b);
+void output(int a, int b, int sum);
 
 main() {
   int a,b,sum;
@@ -19,12 +19,12 @@ int input()  {
   return digit;
 }
 
-int add(int num1, int num2) {
+int add(int a, int b) {
   int solve;
-  solve = num1 + num2;
+  solve = a + b;
   return solve;
 }
 
-void output(int element1, int element2, int solution) {
-  printf("The sum of %d and %d is %d", element1, element2, solution);
+void output(int a, int b, int sum) {
+  printf("The sum of %d and %d is %d", a, b, sum);
 }
