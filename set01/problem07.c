efbe7636d8dca4cc5fca64fb228f520077787f34
Adithi  Vaidyanathan <114479214+Adithi19@users.noreply.github.com>
2022-10-13 10:51:10 +0000

    Problem solved

---
+++ set01/problem07.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int input_n();
+int sum_n_nos(int);
+void output(int n, int sum);
+
+main() {
+  int n, sum;
+  n=input_n();
+  sum=sum_n_nos(n);
+  output(n,sum);   
+}
+
+int input_n() {
+  int digit;
+  printf("Enter the number till where sum has to be found\n");
+  scanf("%d", &digit);
+  return digit;
+}
+
+int sum_n_nos(int x) {
+  int add,i;
+  add=0;
+  for(i=1; i<=x; i++) {
+    add=i+add;
+  }
+  return add;
+  }
+
+void output(int n, int sum) {
+  printf("The sum of natural numbers till %d is %d",n,sum);
+}
