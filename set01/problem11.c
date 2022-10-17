c4c87eb8f6537e1d81dcff2c4b1f24d6bc5ff9b3
Adithi  Vaidyanathan <114479214+Adithi19@users.noreply.github.com>
2022-10-17 13:49:45 +0000

    Problem solved

---
+++ set01/problem11.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+struct _complex {
+	float real;
+	float imaginary;
+};
+typedef struct _complex Complex;
+
+Complex input_complex();
+Complex add_complex(Complex a, Complex b);
+void output(Complex a, Complex b, Complex sum);
+
+main() {
+  Complex a,b,sum;
+  a=input_complex();
+  b=input_complex();
+  sum=add_complex(a,b);
+  output(a,b,sum);
+}
+
+Complex input_complex() {
+  Complex digit;
+  printf("Enter complex number:");
+  scanf("%f%f",&digit.real,&digit.imaginary);
+  return digit;
+}
+
+Complex add_complex(Complex a, Complex b) {
+  Complex add;
+  add.real=a.real+b.real;
+  add.imaginary=a.imaginary+b.imaginary;
+  return add;
+}
+
+void output(Complex a, Complex b, Complex sum) {
+  printf("The sum of %f+%fi and %f+%fi is %f+%fi",a.real,a.imaginary,b.real,b.imaginary,sum.real,sum.imaginary);
+}
+
